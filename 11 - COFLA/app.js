db88fe74c5dd236359b0d0ffa684cb43b0f99296
Dani <danielmateu86@gmail.com>
2022-03-21 12:27:54 +0100

    Datos creados

--- 11 - COFLA/app.js
+++ 11 - COFLA/app.js
@@ -1,2 +1,44 @@
 'use strict'
 
+//Creamos los datos de la
+const alumno = [{
+        nombre: 'Daniel Mateu',
+        email: 'daniel@gmail.com',
+        materia: 'Fisica 3'
+    },
+    {
+        nombre: 'Silvia Cazorla',
+        email: 'Silvia@gmail.com',
+        materia: 'Derecho 3'
+    },
+    {
+        nombre: 'Nuk Peluk',
+        email: 'Peluk@gmail.com',
+        materia: 'Comida 3'
+    },
+    {
+        nombre: 'John Snow',
+        email: 'Snow@gmail.com',
+        materia: 'Fisica 3'
+    },
+    {
+        nombre: 'Link',
+        email: 'link@gmail.com',
+        materia: 'Fisica 3'
+    },
+    {
+        nombre: 'Steve Harris',
+        email: 'daniel@gmail.com',
+        materia: 'Fisica 3'
+    },
+    {
+        nombre: 'Johan Sebastian',
+        email: 'sebas@gmail.com',
+        materia: 'Fisica 3'
+    },
+    {
+        nombre: 'Mike Portnoy',
+        email: 'Mike@gmail.com',
+        materia: 'Fisica 3'
+    }
+]
