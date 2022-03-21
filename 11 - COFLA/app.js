8e8213d915d2d224312457bead1f474835f03ce2
Dani <danielmateu86@gmail.com>
2022-03-21 12:22:51 +0100

    Estilos hechos

--- 11 - COFLA/app.js
+++ 11 - COFLA/app.js
@@ -1,3 +1,2 @@
 'use strict'
 
-//* Interfaz agradeble y atrctiva, debe contener los datos de manera estructurada, ls profesores deben poder elegir cuando rinde el alumno y deben poder actualizar y reemplazar
