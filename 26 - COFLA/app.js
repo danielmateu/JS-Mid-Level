f11ac36efefa6368f1ba98029fc92447016bbdc7
Dani <danielmateu86@gmail.com>
2022-03-24 13:32:03 +0100

    DONE!

--- 26 - COFLA/app.js
+++ 26 - COFLA/app.js
@@ -1,2 +1,21 @@
 'use strict'
 
+const getInfo = async()=>{
+
+    let resultado = await axios('informacion.txt');
+    let aprobados = resultado.data.aprobados;
+    let suspendidos = resultado.data.suspendidos;
+
+    let div = document.createElement('div');
+    div.innerHTML = `
+    Aprobados: ${aprobados}<br>
+    Suspendidos: ${suspendidos}<br>
+    `;
+
+    document.body.appendChild(div)
+}
+
+document.querySelector('.get-info').addEventListener('click',getInfo);
+
+
+
--- 26 - COFLA/readMe.md
+++ 26 - COFLA/readMe.md
@@ -1,4 +1,1 @@
-Fetch y Axios Con Async Await -> Lo usaremos para realizar funciones a las que podamos mandar y de las que podamos recibir peticiones
-
-Importancia del try catch
-
+Cuantas personas cursan el semestre y cuantas no, mediante un API a la cual tenemos acceso.
